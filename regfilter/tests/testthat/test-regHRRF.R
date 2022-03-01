39c79b13287e73a30157ad44d58dc500bc607bbd
Juan Martín <juanmartin@usal.es>
2022-03-01 19:01:40 +0100

    upload package

---
+++ regfilter/tests/testthat/test-regHRRF.R
@@ -0,0 +1,25 @@
+# load the dataset
+data(rock)
+set.seed(9)
+out <- regHRRF(x = rock[,-ncol(rock)], y = rock[,ncol(rock)])
+
+# tests
+test_that("the result has the correct class", {
+  expect_s3_class(out, "rfdata")})
+
+test_that("the number of idclean plus idnoise equals the number of dataset samples", {
+      expect_true(length(out$idclean) + length(out$idnoise) == nrow(rock))
+})
+
+test_that("the idclean and idnoise are equal to dataset rownames", {
+  expect_true(any(sort(c(out$idclean,out$idnoise)) == as.integer(rownames(rock))))
+})
+
+test_that("the original dataset can be correctly reconstructed from the rfdata object", {
+      dataClean <- cbind(out$xclean, out$yclean)
+      dataNoisy <- cbind(out$xnoise, out$ynoise)
+      colnames(dataClean) = colnames(dataNoisy) = colnames(rock)
+      processData <- rbind(dataClean, dataNoisy)
+      processData <- processData[order(as.numeric(row.names(processData))), ]
+      expect_equal(processData, rock)
+})
